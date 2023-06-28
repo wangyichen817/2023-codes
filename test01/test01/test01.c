0bc0559e8fa814bf9d18e2716bf445dc318faa51
wang_yi_chen817 <1974207572@qq.com>
2023-06-28 15:39:35 +0800

    开始学习

---
+++ test01/test01/test01.c
@@ -0,0 +1,95 @@
+#define CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d %d", &a,&b);
+//	if (a > b)
+//		printf("%d\n", a);
+//	else
+//		printf("%d\n", b);
+//}
+//#include <stdio.h>
+//
+//int main() {
+//    int n;
+//    scanf_s("%d", &n);
+//    int s;
+//    int a = n / 12;
+//    //a=ÌËÊý
+//    int b = 4 * a;
+//    //b=Ê±¼ä
+//
+//    s = b + 2;
+//    printf("%d\n", s);
+//    return 0;
+//}
+
+
+int main() {
+//    
+//    int a, b, c;
+//    scanf_s("%d,%d,%d", &a, &b, &c);
+//    if (a + b > c && a + c > b && b + c > a)
+//    {
+//        if (a == b && b== c)
+//        {
+//            printf("Equilateral triangle!\n");
+//        }
+//        else if (a == b || a == c || b == c)
+//        {
+//            printf("Isosceles triangle!\n");
+//        }
+//        else
+//        {
+//            printf("Ordinary triangle!\n");
+//        }
+//    }
+//    else
+//    {
+//        printf("Not a triangle!\n");
+//    }
+ /*   float a , b;
+    scanf_s("%d,%d", &a, &b);
+    float d =  (b/100)* (b / 100);
+    printf("%.2f", a/d);
+    return 0;
+}*/
+//#include <stdio.h>
+//int main()
+//{
+//    int a = 1, b = 1, c = 1;
+//    scanf_s("%d %d %d", &a, &b, &c);
+//   
+//        if (a + b > c && a + c > b && b + c > a)
+//        {
+//            if (a == b && b == c)
+//            {
+//                printf("Equilateral triangle!\n");
+//            }
+//            else if (a == b || b == c || a == c)
+//            {
+//                printf("Isosceles triangle!\n");
+//            }
+//            else
+//            {
+//                printf("Ordinary triangle!\n");
+//            }
+//        }
+//        else
+//        {
+//            printf("Not a triangle!\n");
+//        }
+//    
+    int a,b;
+        scanf_s("%d", &a);
+        b = a*31560000;
+        printf("%d\n", b);
+
+    return 0;
+}
+
+
+
+
